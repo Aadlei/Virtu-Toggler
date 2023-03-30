0f85a2cc8547e725322ceba531877ed703df878c
Ådne-Sigurd Frantzen Leirdal <asleirdal@uia.no>
2023-03-30 15:27:37 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿using System.Management.Automation;
+
+using var ps = PowerShell.Create();
+
+ps.AddScript("(Get-ItemProperty -Path \"HKLM:\\SYSTEM\\CurrentControlSet\\Control\\DeviceGuard\\Scenarios\\HypervisorEnforcedCodeIntegrity\")").Invoke();
